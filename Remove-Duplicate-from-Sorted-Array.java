30945c68769f123f55086d679c81aa3a7429da13
Anuj Jain <anujjainbatu@gmail.com>
2023-07-01 18:34:27 +0530

    Update Remove-Duplicate-from-Sorted-Array.java
    Runtime
    1 ms
    Memory
    43.8 MB

--- Remove-Duplicate-from-Sorted-Array.java
+++ Remove-Duplicate-from-Sorted-Array.java
@@ -1,20 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int index = 0;
-        if (nums.length == 1) {
-            index++;
-            return index;
-        }
-        for (int i = 0; i < nums.length-1; i++) {
-            nums[index] = nums[i];
-            index++;
-            while (i < nums.length-1 && nums[i] == nums[i+1]) {
-                i++;
+        int index = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[index] = nums[i];
+                index++;
             }
-        }
-        if (nums.length > 1 && nums[nums.length-1] != nums[nums.length-2]) {
-            nums[index] = nums[nums.length-1];
-            index++;
         }
         return index;
     }
