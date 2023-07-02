4ae07bac39e6ea803e55e1f3e39f9c94b6e97fdc
Anuj Jain <anujjainbatu@gmail.com>
2023-07-02 10:31:52 +0530

    Update Majority-Element.java
    Runtime
    2 ms
    Memory
    48.7 MB

--- Majority-Element.java
+++ Majority-Element.java
@@ -1,18 +1,8 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int major_mark = nums.length/2;
-        for (int i = 0; i < nums.length; i++) {
-            int count = 1;
-            for (int j = i+1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-            if (count > major_mark) {
-                return nums[i];
-            }
-        }
-        return 0;
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
     }
 }
 
