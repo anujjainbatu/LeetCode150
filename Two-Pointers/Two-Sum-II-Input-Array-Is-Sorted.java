3cf3a21e0210d80d5b538e0a5b074d11da0f9fff
Anuj Jain <anujjainbatu@gmail.com>
2023-07-05 08:55:56 +0530

    Update Two-Sum-II-Input-Array-Is-Sorted.java
    Trash remove

--- Two-Pointers/Two-Sum-II-Input-Array-Is-Sorted.java
+++ Two-Pointers/Two-Sum-II-Input-Array-Is-Sorted.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int[] result = {0,0};
         for (int i = 0; i < numbers.length; i++) {
             for (int j = i+1; j < numbers.length; j++) {
                 if (numbers[i] + numbers[j] == target) {
